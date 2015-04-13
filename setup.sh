99f67bed2038d18edd8d73b2e8328770e1cfbba9
Peter Todd <pete@petertodd.org>
2015-04-13 18:24:31 -0400

    flesh out setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-# Setup symlinks in $HOME
+# Setup symlinks in $HOME, overwriting existing stuff
 
+for DOTFILE in \
+    .bash_logout \
+    .bash_profile \
+    .bashrc \
+    .inputrc \
+    .ssh \
+    .vim \
+    .vimoutlinerrc \
+    .vimrc
+do
+    ln -f -s -t $HOME dotfiles/$DOTFILE
+done
